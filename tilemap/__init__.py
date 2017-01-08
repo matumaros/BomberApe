ddf384be4ae827586ac34da8f5c340ba47f521cd
Matthias Schreiber <mat@boar.bar>
2017-01-08 14:48:25 +0100

    tiles scale properly now and are at the right position

--- tilemap/__init__.py
+++ tilemap/__init__.py
@@ -12,25 +12,33 @@ class TileMap(RelativeLayout):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.tiles = {}
-        self.focused_pos = 0, 0  # x, y of focus
-        self.scale = (64, 64)
+        self.bind(focused_pos=lambda wg, pos: self.on_focused_pos(pos))
+
+    def coord_to_pos(self, coord):
+        norm = tuple(map(int, coord.split('|')))
+        x = norm[0] * self.scale + self.focused_pos[0]
+        y = norm[1] * self.scale + self.focused_pos[1]
+        return x, y
 
     def update_tiles(self, tiles):
-        for pos, tile_type in tiles.items():
+        for coord, tile_type in tiles.items():
             try:
-                tile = self.tiles[pos]
+                tile = self.tiles[coord]
             except KeyError:
                 pass
             else:
                 self.remove_widget(tile)
             tile = Tile(
                 source='atlas://atlases/tiles/{}'.format(tile_type),
-                pos=pos,
-                size=self.scale,
+                pos=self.coord_to_pos(coord),
+                size=(self.scale, self.scale),
             )
-            self.tiles[pos] = tile
+            self.tiles[coord] = tile
             self.add_widget(tile)
+
+    def on_focused_pos(self, fpos):
+        for coord, tile in self.tiles.items():
+            tile.pos = self.coord_to_pos(coord)
 
     def load_map(self, name):
         return  # needs to be moved to loading module
