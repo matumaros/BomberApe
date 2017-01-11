e40c525e3e973c4b4ac6be2dde02c4c6fdf39a0c
Matthias Schreiber <mat@boar.bar>
2017-01-11 13:47:31 +0100

    moved example setup for game into own module

---
+++ start_game.py
@@ -0,0 +1,24 @@
+
+
+from uuid import uuid4
+
+from main import BomberApe
+from screens.game import Game
+from controllers.player import Player
+from controllers.server import Server
+
+ba = BomberApe()
+game = Game(name='game')
+ba.view.add_widget(game)
+# Example game setup
+server = Server()
+server.load_map('content/maps/new.map')
+player = Player(uuid4(), server)
+server.players = {
+    player.uid: player,
+}
+euid = uuid4()
+server.spawn_entity('gorilla', euid, player.uid)
+game.start(player)
+###
+ba.run()
